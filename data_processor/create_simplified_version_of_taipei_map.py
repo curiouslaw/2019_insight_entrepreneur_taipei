763d63122e809f24a81fab052e080305b09e5b08
curiouslaw <laurent.saerang@gmail.com>
2020-10-11 22:40:05 +0800

    Make code to simplified and make taipei map size smaller

--- data_processor/create_simplified_version_of_taipei_map.py
+++ data_processor/create_simplified_version_of_taipei_map.py
@@ -12,16 +12,25 @@ if __name__ == '__main__':
     village_shp_filepath = get_shp_filepath(os.path.join(data_dir, 'taiwan_twd97_map_data_village'))
 
     data_mart_dirpath = os.path.join(data_dir, 'aggregated-data_mart')
-    output_filepath = os.path.join(data_mart_dirpath, 'simplified_taipei_village.geojson')
+    output_village_filepath = os.path.join(data_mart_dirpath, 'simplified_taipei_village.geojson')
+    output_township_filepath = os.path.join(data_mart_dirpath, 'simplified_taipei_township.geojson')
 
     village_gpd = load_normalize_gov_shp_data(village_shp_filepath)
 
     taipei_village_gpd = village_gpd[village_gpd['county_chinese_name'] == '臺北市']
     taipei_village_gpd.set_index('village_code', drop=False, inplace=True)
 
+    taipei_township_gpd = taipei_village_gpd[['township_code', 'geometry']].dissolve(by='township_code')
+
     # select tolerance of 0.05km, 1 point is about 111 km
     tolerance = 0.05 / 111
 
-    simplified_gpd = taipei_village_gpd.copy()
-    simplified_gpd['geometry'] = taipei_village_gpd.simplify(tolerance, preserve_topology=False)
-    simplified_gpd[['geometry']].to_file(output_filepath, driver='GeoJSON')
+    # save for village detail geojson
+    simplified_village_gpd = taipei_village_gpd.copy()
+    simplified_village_gpd['geometry'] = simplified_village_gpd.simplify(tolerance, preserve_topology=False)
+    simplified_village_gpd[['geometry']].to_file(output_village_filepath, driver='GeoJSON')
+
+    # save for township detail geojson
+    simplified_township_gpd = taipei_township_gpd.copy()
+    simplified_township_gpd['geometry'] = simplified_township_gpd.simplify(tolerance, preserve_topology=False)
+    simplified_township_gpd[['geometry']].to_file(output_township_filepath, driver='GeoJSON')
